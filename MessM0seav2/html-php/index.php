6969bc3be0128037e0cb31a6d07ffd19d2873490
Maxime Chapuis <maxime.chapuis@unistra.fr>
2024-12-05 22:45:42 +0100

    Ajout de la structure de l'appli

---
+++ MessM0seav2/html-php/index.php
@@ -0,0 +1,31 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Votre Nom">
+    <title>M3SSM0SEA</title>
+    <!-- Ajouter des styles -->
+    <link rel="stylesheet" href="../css/index.css">
+    <script src="../js/index.js"></script>
+</head>
+
+<body>
+
+<header>
+    <div>
+        <h1>MESSMOSEA : Corps Humain et Océan</h1>
+        <p>Un parallèle entre les systèmes biologiques et marins</p>
+    </div>
+</header>
+
+<main>
+    <!-- Section d'accueil -->
+    <section id="StartButton" class="organ-section text-center">
+        <button>Commencer l'aventure</button>
+    </section>
+</main>
+
+<footer>
+    <p>Créé par la M3SSM0TEAM | © 2024 Tous droits réservés</p>
+</footer>
+
+</body>
+</html>
