a048511d5faecca442f11d1b973e04d8f9712788
ghieber <gaetan.hieber@etu.iutrs.unistra.fr>
2024-12-06 02:42:45 +0100

    fichiers de gestion des cartes

---
+++ MessM0seav2/js/cards.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', function() {
+        const cardInner = card.querySelector('.card-inner');
+        cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
+    });
+});
